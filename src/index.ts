c8cc3f7adbc3a09b2f49dcffd228ca5773b3965c
Bekzat <aqparaqsha@gmail.com>
2026-04-20 14:27:01 +0500

    reschedule

--- src/index.ts
+++ src/index.ts
@@ -109,6 +109,6 @@ async function run() {
 }
 
 // Schedule the highlight sending at 14:20 KZ = 09:20 UTC
-cron.schedule("23 09 * * *", () => {
+cron.schedule("35 09 * * *", () => {
   run().catch(console.error);
 });
