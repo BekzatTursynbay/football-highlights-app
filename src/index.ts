a21dabe6e3c0467160792428cc83d2ca9fe955e0
Bekzat <aqparaqsha@gmail.com>
2026-04-21 21:01:45 +0500

    old version

--- src/index.ts
+++ src/index.ts
@@ -109,7 +109,7 @@ async function run() {
 }
 
 // Schedule the highlight sending at 14:20 KZ = 09:20 UTC
-cron.schedule("51 13 * * *", () => {
+cron.schedule("03 16 * * *", () => {
   run().catch(console.error);
 });
 // run().catch(console.error);
